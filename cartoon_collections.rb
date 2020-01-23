91e9c0002e5f8807b768058da2ac885277c89305
Justin Bermudez <justinanbermudez@gmail.com>
2020-01-23 06:40:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer)
-  summon = planeteer.map{ |string| string.capitalize() + "!"}
-  puts summon
+  planeteer.map{|captain| captain.capitalize() + "!"}
 end
 
 def long_planeteer_calls(array)
