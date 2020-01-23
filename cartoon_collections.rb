3d85a51a9409be45ffb4cfae0283aa60c849da04
Justin Bermudez <justinanbermudez@gmail.com>
2020-01-23 06:31:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,10 @@ def summon_captain_planet(planeteer)
 end
 
 def long_planeteer_calls(array)
-  bool = true
+  bool = false
   array.each do |call|
     if call.length > 4
-      bool = false
+      bool = true
     end
   end
   bool
